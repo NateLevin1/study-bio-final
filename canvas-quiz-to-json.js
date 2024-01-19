dd65352ece0b1c0aadea5c47654160e897a580be
NateLevin1 <63370104+UltimatePro-Grammer@users.noreply.github.com>
2024-01-19 15:07:52 -0500

    ✨ Allow canvas-quiz-to-json.js to work with non-answer pages

--- canvas-quiz-to-json.js
+++ canvas-quiz-to-json.js
@@ -1,47 +1,60 @@
 // open a canvas quiz page and run this code in the browser console.
 (function () {
-    const questions = document.getElementsByClassName("question"),
-        output = [];
+    const questions = document.getElementsByClassName("question");
+    const output = [];
     for (const question of questions) {
         if (!question.classList.contains("multiple_choice_question")) {
             console.warn("Encountered non-multiple choice question. Skipping!");
             continue;
         }
-        const incorrect = question.classList.contains("incorrect");
-        const id = question.id.replace("question_", ""),
-            gotCorrect = question.classList.contains("correct"),
-            questionText = question
+        const id = question.id.replace("question_", "");
+        const gotCorrect = question.classList.contains("correct");
+        const questionText = question
+            .querySelector(".text")
+            .querySelector(":scope > .question_text")
+            .textContent.trim();
+        const questionImages = Array.from(
+            question
                 .querySelector(".text")
                 .querySelector(":scope > .question_text")
-                .textContent.trim(),
-            questionImages = Array.from(
-                question
-                    .querySelector(".text")
-                    .querySelector(":scope > .question_text")
-                    .querySelectorAll("img")
-            ).map((img) => img.src),
-            answers = question
-                .querySelector(".text")
-                .querySelector(":scope > .answers")
-                .querySelector(".answers_wrapper")
-                .querySelectorAll(".answer"),
-            answerList = [];
+                .querySelectorAll("img")
+        ).map((img) => img.src);
+
+        const answersDiv = question
+            .querySelector(".text")
+            .querySelector(":scope > .answers");
+        const onAnswersPage = !!answersDiv.querySelector(".answers_wrapper");
+        const answers = answersDiv.querySelectorAll(".answer");
+        const answerList = [];
         for (const answer of answers) {
-            // canvas bug that sometimes results in empty extra answers
-            const selectAnswer = answer.querySelector(".select_answer");
-            if (selectAnswer == null) continue;
+            if (onAnswersPage) {
+                // canvas bug that sometimes results in empty extra answers
+                const selectAnswer = answer.querySelector(".select_answer");
+                if (selectAnswer == null) continue;
 
-            const label = selectAnswer.querySelector("label");
+                const label = selectAnswer.querySelector("label");
 
-            const text =
+                const text = (
                     label.querySelector(".answer_text").textContent ||
-                    label.querySelector(".answer_html").textContent,
-                correct = answer.classList.contains("correct_answer");
-            answerList.push({
-                text: text,
-                correct: correct,
-                selected: selectAnswer.querySelector("input").checked,
-            });
+                    label.querySelector(".answer_html").textContent
+                ).trim();
+                const correct = answer.classList.contains("correct_answer");
+                answerList.push({
+                    text: text,
+                    correct: correct,
+                    selected: selectAnswer.querySelector("input").checked,
+                });
+            } else {
+                // on test page
+                const text = answer
+                    .querySelector(".answer_label")
+                    .textContent.trim();
+
+                answerList.push({
+                    text: text,
+                    correct: false, // this is handled by the "edit" functionality of the frontend
+                });
+            }
         }
         if (answerList.length === 0) {
             console.warn("Encountered question with no answers. Skipping!");
@@ -50,11 +63,8 @@
         }
         const noCorrectAnswer = answerList.every((answer) => !answer.correct);
         if (noCorrectAnswer) {
-            if (incorrect) {
-                console.warn(
-                    "Encountered question with no correct answer and the question was marked incorrect. Skipping!"
-                );
-                continue;
+            if (!gotCorrect) {
+                console.warn("Encountered question with no correct answer");
             } else {
                 // correct is selected
                 answerList.forEach((answer) => {
@@ -77,5 +87,10 @@
     }
     console.log(output);
     const outputString = JSON.stringify(output);
-    prompt('Press "Ctrl+C, Enter" to Copy to Clipboard:', outputString);
+    document.body.innerHTML = `<h1>Click anywhere to copy questions to clipboard</h1> <pre>${outputString}</pre>`;
+    document.body.onclick = async () => {
+        await navigator.clipboard.writeText(outputString);
+        document.body.innerHTML = `<h1>Copied to clipboard!</h1>`;
+    };
+    document.body.scrollTop = 0;
 })();
